fc5512f20df7943a34a6490d212bade99bdce41e
Nicolas FAUGEROUX <nicolas.faugeroux@pm.me>
2022-12-26 11:10:00 +0100

    adding template to clone for each day

---
+++ template/main.py
@@ -0,0 +1,21 @@
+input_file = "input.txt"
+
+
+
+def first_part():
+    with open(input_file, 'r') as file:
+        pass
+
+def second_part():
+    with open(input_file, 'r') as file:
+        pass
+
+
+def main():
+  
+
+    first_result = first_part()
+    print("The first result is {}".format(first_result))
+
+    second_result = second_part()
+    print("The second result is {}".format(second_result))
